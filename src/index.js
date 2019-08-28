1324c063a429256eec33e6e1b677c8c9ed7dfee9
moisessbr <moisess@outlook.com>
2019-08-28 15:45:46 -0300

    Iniciando Projeto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+import Routes from './routes';
+
+export default function App() {
+  return <Routes />;
+}
